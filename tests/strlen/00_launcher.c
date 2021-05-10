48795ae24c895ef178d926e7e62e9438664861bc
Junichi Tanaka <junichi200123@gmail.com>
2021-05-11 00:39:23 +0900

    puts()使わない

--- tests/strlen/00_launcher.c
+++ tests/strlen/00_launcher.c
@@ -6,7 +6,7 @@ int	strlen_launcher(void)
 	t_unit_test	*testlist;
 
 	testlist = NULL;
-	puts("STRLEN:");
+	ft_putendl_fd("STRLEN:\n", STDOUT_FILENO);
 	load_test(&testlist, "Basic test", &basic_test);
 	load_test(&testlist, "NULL test", &null_test);
 	load_test(&testlist, "Bigger string test", &bigger_str_test);
